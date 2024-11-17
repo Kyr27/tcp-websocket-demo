76e6a4990a70aca8fe811ac50ed259e09d903486
Kyr27 <001eff.dks@gmail.com>
2024-11-17 12:35:50 +0100

    added necessary includes for websocketpp and a define that tells it not use boost

--- tcp-websocket-demo/src/main.cpp
+++ tcp-websocket-demo/src/main.cpp
@@ -1,6 +1,9 @@
-#define ASIO_STANDALONE
+#define ASIO_STANDALONE // using asio without boost
+#define _WEBSOCKETPP_CPP11_STL_ // tell websocketpp that we don't need boost stuff, as it's standard in C++ 11 and after
 #include <iostream>
 #include "asio.hpp"
+#include "websocketpp/config/asio_no_tls.hpp"
+#include "websocketpp/server.hpp"
 
 
 int main()
