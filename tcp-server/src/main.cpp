a3e240876f39913ccd68576a1f7eb8bc983f9864
Kyr27 <001eff.dks@gmail.com>
2024-11-17 15:49:56 +0100

    added shutdown handling

--- tcp-server/src/main.cpp
+++ tcp-server/src/main.cpp
@@ -1,13 +1,69 @@
 #define ASIO_STANDALONE // using asio without boost
 #define _WEBSOCKETPP_CPP11_STL_ // tell websocketpp that we don't need boost stuff, as it's standard in C++ 11 and after
-#define _WIN32_WINNT 0x0A00 // Windows 10
+
+#ifdef _WIN32
+	#define _WIN32_WINNT 0x0A00 // Windows 10
+	#define WIN32_LEAN_AND_MEAN
+	#include <Windows.h>
+#endif
 
 #include <iostream>
+#include <stdlib.h>
+#include <csignal>
+#include <atomic>
 #include "asio.hpp"
 #include "websocketpp/config/asio_no_tls.hpp"
 #include "websocketpp/server.hpp"
 
 typedef websocketpp::server<websocketpp::config::asio> server;
+
+void shutdown();
+void OnMessage(server* wsServer, websocketpp::connection_hdl hdl, server::message_ptr msg);
+void signal_handler(int signum);
+BOOL WINAPI ConsoleHandler(DWORD dwCtrlType);
+
+static server wsServer; // needs to be global so that we can control it from the console handler
+std::atomic<bool> wsServerRunning(true);
+int main()
+{
+	// cross platform way of handling normal application exits, the extent "normal" depends on the platform
+	atexit(shutdown);
+
+	// Register the shutdown handlers with proper functions depending on the platform
+	#ifdef _WIN32
+		// Register Console Control Handler if running on Windows
+		if (!SetConsoleCtrlHandler(ConsoleHandler, true))
+		{
+			std::cerr << "Failed to set console control handler\n";
+			return 1;
+		}
+	#else
+		// Use signals for other platforms (less versatile)
+		std::signal(SIGINT, signal_handler);  // handle Ctrl + C gracefully
+		std::signal(SIGTERM, signal_handler); // handle termination gracefully
+	#endif
+
+	// Initialize websocketpp using standalone asio
+	wsServer.init_asio();
+
+	// Call OnMessage function when the server receives a message
+	wsServer.set_message_handler(std::bind(&OnMessage, &wsServer, std::placeholders::_1, std::placeholders::_2));
+
+	// Listen on localhost with port 9002 for messages
+	wsServer.listen(9002);
+
+	// Start
+	wsServer.start_accept();
+
+	std::cout << "C++ - Server Online\n";
+
+	while (wsServerRunning.load())
+	{
+		wsServer.run_one();
+	}
+
+	return 0;
+}
 
 void OnMessage(server* wsServer, websocketpp::connection_hdl hdl, server::message_ptr msg)
 {
@@ -17,31 +73,34 @@ void OnMessage(server* wsServer, websocketpp::connection_hdl hdl, server::messag
 	std::cout << "C++ - Sent message to C#\n";
 }
 
-int main()
-{
-	server wsServer;
-
-	// Enable logging (seems to be set to this by default)
-	//wsServer.set_access_channels(websocketpp::log::alevel::all);
-
-	// Don't show messages that we are sending to the client
-	//wsServer.clear_access_channels(websocketpp::log::alevel::frame_payload);
-
-	// Initialize websocketpp using standalone asio
-	wsServer.init_asio();
-
-	// Call OnMessage function when the server receives a message
-	wsServer.set_message_handler(std::bind(&OnMessage, &wsServer, std::placeholders::_1, std::placeholders::_2));
-
-	// Listen on port 8788 for messages
-	wsServer.listen(9002);
-
-	// Start
-	wsServer.start_accept();
-	wsServer.run();
-
-	std::cout << "C++ - Server Online\n";
-
-	static_cast<void>(getchar());
-	return 0;
+void signal_handler(int signum) {
+	shutdown();
 }
+
+void shutdown()
+{
+	std::cout << "Shutting down server...\n";
+	wsServerRunning.store(false);
+	wsServer.stop_listening();
+	wsServer.stop();
+
+	std::cout << "Server has shutdown\n";
+}
+
+#ifdef _WIN32
+	BOOL WINAPI ConsoleHandler(DWORD dwCtrlType)
+	{
+		switch (dwCtrlType) {
+		case CTRL_C_EVENT:
+		case CTRL_BREAK_EVENT:
+		case CTRL_CLOSE_EVENT:
+		case CTRL_LOGOFF_EVENT:
+		case CTRL_SHUTDOWN_EVENT:
+			shutdown();
+
+			return TRUE;
+		default:
+			return FALSE;
+		}
+	}
+#endif
