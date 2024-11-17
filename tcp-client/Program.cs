a04e5fa0a5d71bc85201b45b3951e7de65c32f31
Kyr27 <001eff.dks@gmail.com>
2024-11-17 14:19:05 +0100

    Moved ReadKey into the using block to prevent the connection from being closed immediately

--- tcp-client/Program.cs
+++ tcp-client/Program.cs
@@ -5,16 +5,16 @@ internal class Program
     private static void Main(string[] args)
     {
         // using here ensures that if anything goes wrong, it is properly disposed of and not left hanging
-        using (WebSocket webSocket = new WebSocket("ws://localhost:8788"))
+        using (WebSocket webSocket = new WebSocket("ws://localhost:9002"))
         {
             webSocket.OnMessage += WebSocket_OnMessage;
 
             webSocket.Connect();
             webSocket.Send("C# - Hello Server!");
-        }
 
-        Console.WriteLine("C# - Client Online");
-        Console.ReadKey();
+            Console.WriteLine("C# - Client Online");
+            Console.ReadKey(true);
+        }
     }
 
     private static void WebSocket_OnMessage(object? sender, MessageEventArgs e)
